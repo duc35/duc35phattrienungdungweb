8158152a4623f8e0f73bd994f7b2f0f3cb6c3cc8
duc35 <hoangduc14142003@gmail.com>
2023-11-08 16:17:40 +0700

    'slide34

---
+++ php/slide34.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8"> 
+        <title>duc</title>
+</head>
+<body>
+    <form action="5-formget.php" method="get">
+        <p>Name: </p>
+        <input type="text" name="name"><br>
+        <p>E-mail: </p>
+        <input type="text" name="email"><br>
+        <p>đức</p>
+        <input type="submit">
+</form>
+</body>
+</html>
+    
+
