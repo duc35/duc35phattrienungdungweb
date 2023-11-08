bab4f63dbdf70204f62eee9095003f9f5ce360b2
duc35 <hoangduc14142003@gmail.com>
2023-11-08 15:43:38 +0700

    'slide17

---
+++ php/slide17.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en"dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>duc</title>
+</head>
+<body>
+    <?php
+         echo "my first php";
+         echo "<h3>hello</h3>";
+         echo "<select>";
+         echo "<option>1</option>";
+         echo "<option>2</option>";
+         echo "</select>";
+     ?>
+  </body>
+</html>
+
+     
