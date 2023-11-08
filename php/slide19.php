ee6f03c4ebddf11afefc4b108031393323a29999
duc35 <hoangduc14142003@gmail.com>
2023-11-08 15:49:58 +0700

    'slide19

---
+++ php/slide19.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>duc</title>
+</head>
+<body>
+    <?php
+         echo "My first PHP";
+         ?>
+         <h3>hello đức</h3>
+         <select>
+            <option>1</option>
+            <option>2</option>
+       </select>
+   </body>
+</html>
+ 
+        
