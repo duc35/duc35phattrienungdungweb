26f5377a0a661ec3874a5c511e0590f3d3e45d59
duc35 <hoangduc14142003@gmail.com>
2023-11-08 15:30:34 +0700

    'sile13

--- php/slide13.php
+++ php/slide13.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+      echo " MY first PHP FILE";
+      echo "<h3> hello đức</h3>";
+      echo "<select>";
+      echo "<option>1</option>";
+      echo "<option>2</option>";
+      echo "</select>";
+?>
