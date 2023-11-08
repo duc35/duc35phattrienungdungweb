bcbbfb75bbe7cb08e83028c98f8362cc1996a127
duc35 <hoangduc14142003@gmail.com>
2023-11-08 16:03:15 +0700

    'slide25

---
+++ php/slide25.php
@@ -0,0 +1,12 @@
+<?php
+     $subject = "phát triển UDWEB";
+     $unniversityName = "HUMG";
+     $firstNumber = 2;
+     $secondNumber = 2;
+
+     echo "học ". $subject . "phòng B205 " . $unniversityName . "<br>";
+     echo "học  $subject ở phòng B205 tại  $unniversityName <br>";
+     echo "<h1> $unniversityName </h1>";
+     echo $firstNumber + $secondNumber;
+?>
+     đức
