a853a0eb5563efe36736653c5b6aa0743081ae16
duc35 <hoangduc14142003@gmail.com>
2023-11-22 14:59:58 +0700

    'slide39

---
+++ php/slide39.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="1tr">
+    <head>
+        <meta charset="utf-8">
+        <title>duc</title>
+</head>
+<body>
+    <form action="5-formget.php" method="get">
+        <p>Name: </p>
+        <input type="text" name="name"><br>
+        <p>E-mail: </p>
+        <input type="text" name="email"><br>
+        <p>duc</p>
+        <input type="submit">
+</form>
+</body>
+</html>
