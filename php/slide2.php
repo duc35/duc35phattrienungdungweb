67cc6febcc9e1b2929af60d38b554f6eb9a52a1c
duc35 <hoangduc14142003@gmail.com>
2023-11-22 15:11:02 +0700

    'slide2

---
+++ php/slide2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="1tr">
+    <head>
+        <meta charset="utf-8">
+        <title>duc</title>
+</head>
+<body>
+    <?php
+    $T = true;
+    $F = false;
+    echo "t = $T<br>";
+    echo "F = $F<br>";
+
+    $check1 = 10 > 5;
+    $check2 = 20 < 10;
+    $check3 = $check1 && $check2;
+    $check4 = $check1 || $check2;
+    echo "check1 = $check1 <br>";
+    echo "check2 = $check2 <br>";
+    echo "check3 = $check3 <br>";
+    echo "check4 = $check4";
+    ?>
+    </body>
+    </html>
+
