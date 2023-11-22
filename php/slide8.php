f40c15d1d6f28995643f08f49599082875da9f2f
duc35 <hoangduc14142003@gmail.com>
2023-11-22 15:16:30 +0700

    'slide8

---
+++ php/slide8.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="1tr">
+    <head>
+        <meta charset="utf-8">
+        <title>duc</title>
+</head>
+<body>
+    <h3>THOAT GAME?</h3>
+    <form action="8-ifresult.php" method="get">
+        <select name="respone">
+            <option value="yes">có</option>
+            <option value="no">không</option>
+</select>
+<input type="submit" value="submit">
+</form>
+</body>
+</html>
+
