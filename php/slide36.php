3d208b348b5ddae32a99ebb35f98f26e4ae3851b
duc35 <hoangduc14142003@gmail.com>
2023-12-05 19:24:55 +0700

    'slide40

--- php/slide36.php
+++ php/slide36.php
@@ -1,1 +1,11 @@
 <!DOCTYPE html>
+<html lang="en" dir="1tr">
+    <head>
+        <meta charset="utf-8">
+        <title>duc</title>
+</head>
+<body>
+    xinchao <?php echo $_GET["name"]; ?><br>
+    email ban la: <?php echo $_GET["email"]; ?>
+</body>
+</html>
---
+++ php/slide40.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'myDB';
+$conn = new mysqli($servername, $username, $password, $db);
+if ($conn->connection_error)
+{
+  die('Connection failed:' . $conn->connection_error);
+
+}
+echo "Connected successfully <br>";
+?>
+
+</body>
+</html>
